c438bb4fd3f09ee55040de6bd4934dcff4215686
pjvip <140431425+pjvip@users.noreply.github.com>
2023-09-11 19:11:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ class Config:
     DATABASE_URI = os.environ.get("DATABASE_URI", "")  
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluste0")
     COLLECTION_NAME = os.environ.get('COLLECTION_NAME', 'Data')
-    SESSION = os.environ.get("SESSION", "BQCR924ArSg4TglBmRLk0ZtxMnxUZrTU9NEZIWOHeU4us0LV8hsQMt2wBRiwDBfDSiUan84XONY1_jTDVe4AkPF-4K-ICFIKzS6Nvzv_Lg-fbQ0Mg32kRqKXizIYlN1bQ29VBHyLbPkgyB0ho19rg-CqZCQeb4s3wuq1LEobwx667V1rLfHHQpRE0GHD4X4ZZEYFeGvkgLMytPtyRdj9ve1ZaPHQRyHtFo0XNKtl895oxsPiO9-ZeGZ7no2Eg-XkHUZP6XZoFY1iyDVp1fQoKQYwgzIG3qxzXyQcvzkLB6Hs0gLIJnId36ZIptHQ1Vwlln5uCuVvBVrNToVfRcOTTvP8Gu424QAAAAFoyKurAA")   
+    SESSION = os.environ.get("SESSION", "")   
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", ""))
     BOT_USERNAME= os.environ.get("BOT_USERNAME", "")
 
